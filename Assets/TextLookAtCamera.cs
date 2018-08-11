bec90038195beb942134c7de30a62468678170e8
David W <david.wildemann98@gmail.com>
2018-08-11 17:46:03 +0200

    Hop this did not break anything o.o

---
+++ Assets/TextLookAtCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextLookAtCamera : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+        transform.LookAt(Camera.main.gameObject.transform.position);
+
+        transform.Rotate(transform.up, 180);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
